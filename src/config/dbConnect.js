53b138de00897ffb4ab62286cd5adc25fbf864c5
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2023-09-13 23:53:49 -0300

    Configura conexão para pegar nome do banco de variável de ambiente

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.CONNECTION_STRING_DB);
+mongoose.connect(process.env.CONNECTION_STRING_DB, {
+    dbName: process.env.DB
+});
 
 let db = mongoose.connection;
 
